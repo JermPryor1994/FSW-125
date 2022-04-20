f1f0a36f56262556afa17694ae461783fdd731ed
Jeremy Pryor <jeremypryor16@gmail.com>
2022-04-20 09:13:34 -0700

    complete

--- week1/math.js
+++ week1/math.js
@@ -1,16 +1,19 @@
 const addition = (a, b) => {
     return a + b;
 }
-// const subtraction = (a, b) =>{
-//     return a - b;
-// }
-// const multiply = (a, b) =>{
-//     return a * b;
-// }
-// const divide = (a, b) =>{
-//     return a / b;
-// }
+const subtraction = (a, b) =>{
+    return a - b;
+}
+const multiply = (a, b) =>{
+    return a * b;
+}
+const divide = (a, b) =>{
+    return a / b;
+}
 
 module.exports = {
-    addition
+    addition,
+    subtraction,
+    multiply,
+    divide
 }
